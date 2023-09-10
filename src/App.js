a28f76802869a6fd4997aa5e8baefde5d2704ff3
jac <joao.coutinho@sebasi.pt>
2023-09-10 22:04:37 +0100

    feat: dark and light mode added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import {Box} from '@mui/material'
+import { Box, Paper, Switch } from '@mui/material'
 import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles'
+import { MaterialUISwitch } from './utils/MaterialUISwitch';
 
-const App = () => (
+const App = () => {
+    
+    const [mode, setMode] = useState(true);
+
+    const theme = createTheme({
+    palette: {
+        mode: mode ? 'light' : 'dark'
+    }
+    })
+
+    return (
     <BrowserRouter>
-        <Box sx= {{background: '#000' }}> 
-            <Navbar />
-            <Routes>
-                <Route path='/' exact element={<Feed />} />
-                <Route path='/video/:id' element={<VideoDetail />}/>
-                <Route path='/channel/:id' element={<ChannelDetail />}/>
-                <Route path='/search/:searchTerm' element={<SearchFeed />}/>
-            </Routes>
+        <Box> 
+            <Navbar/>
+            <ThemeProvider theme={theme}>
+                <Paper>
+                    <MaterialUISwitch sx={{ position: 'sticky' }} onClick={() => setMode(!mode)}/>
+                    <Routes>
+                        <Route path='/' exact element={<Feed mode={mode}/>} />
+                        <Route path='/video/:id' element={<VideoDetail mode={mode}/>}/>
+                        <Route path='/channel/:id' element={<ChannelDetail mode={mode}/>}/>
+                        <Route path='/search/:searchTerm' element={<SearchFeed />}/>
+                    </Routes>
+                </Paper>
+            </ThemeProvider>
         </Box>
     </BrowserRouter>
-);
+    
+    );
+}
 
 export default App
